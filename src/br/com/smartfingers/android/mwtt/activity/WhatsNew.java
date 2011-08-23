395760cde4e7e248829765d68257860fc072fc5a
ziglee <ziglee@gmail.com>
2011-08-22 22:03:17 -0300

    new whats new screen

--- src/br/com/smartfingers/android/mwtt/activity/WhatsNew.java
+++ src/br/com/smartfingers/android/mwtt/activity/WhatsNew.java
@@ -1,8 +1,13 @@
 package br.com.smartfingers.android.mwtt.activity;
 
+import java.util.Locale;
+
 import br.com.smartfingers.android.mwtt.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.util.Linkify;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class WhatsNew extends Activity {
@@ -14,5 +19,17 @@ public class WhatsNew extends Activity {
 		
 		TextView text = (TextView) findViewById(R.id.sobre_text);
 		text.setText(R.string.whats_new_text);
+    	
+    	LinearLayout sabedoriaLayout = (LinearLayout) findViewById(R.id.sabedoria_layout);
+    	if (Locale.getDefault().equals(new Locale("pt", "BR"))) {
+    		sabedoriaLayout.setVisibility(View.VISIBLE);
+    		TextView sabedoriaText = (TextView) findViewById(R.id.sabedoria_text);
+    		sabedoriaText.setAutoLinkMask(Linkify.ALL);
+    		sabedoriaText.setLinksClickable(true);
+    		sabedoriaText.setText("Conheça nossa mais novo aplicativo 'Sabedoria'." +
+    				"\nAcesse o Android Market https://market.android.com/details?id=net.cassiolandim.sabedoria");
+    	} else {
+    		sabedoriaLayout.setVisibility(View.GONE);
+    	}
 	}
 }
